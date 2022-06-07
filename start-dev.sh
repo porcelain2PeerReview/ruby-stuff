814b29f7f3cb25217e4cab0f22e1d0c9d4c2dade
porcelain2peerreview@protonmail.com <porcelain2peerreview@protonmail.com>
2022-06-07 09:22:57 +0100

    11

--- start-dev.sh
+++ start-dev.sh
@@ -2,4 +2,4 @@
 eval $(ssh-agent)
 ssh-add /home/$USER/$1
 sudo docker build -t rails:dev .
-sudo docker run -itd --name $2 -p 3000:3000 --mount type=bind,source=$SSH_AUTH_SOCK,target=/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent rails:dev
+sudo docker run -itd --name $2 -p 3000:3000 $(readlink -f $SSH_AUTH_SOCK):/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent rails:dev
