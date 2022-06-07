94d1d0f3dba53468a24f1d68b00fe39a70ed51e4
porcelain2peerreview@protonmail.com <porcelain2peerreview@protonmail.com>
2022-06-07 07:39:25 +0100

    added second parameter of keyname to start-dev.sh script

--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 eval $(ssh-agent)
-ssh-add /home/$USER/rails-dev
+ssh-add /home/$USER/$1
 sudo docker build -t rails:dev .
-sudo docker run -itd --name $1 -p 3000:3000 --mount type=bind, source=$SSH_AUTH_SOCK,target=/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent rails:dev
+sudo docker run -itd --name $2 -p 3000:3000 --mount type=bind,source=$SSH_AUTH_SOCK,target=/ssh-agent --env SSH_AUTH_SOCK=/ssh-agent rails:dev
